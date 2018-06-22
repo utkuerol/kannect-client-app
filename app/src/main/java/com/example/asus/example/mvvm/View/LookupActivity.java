e458cd17047dd38c45d0d705dc3da05687948e74
zeineb <zeineb.sboui@yahoo.fr>
2018-06-22 16:12:43 +0200

    add all activities

---
+++ app/src/main/java/com/example/asus/example/mvvm/View/LookupActivity.java
@@ -0,0 +1,15 @@
+package com.example.asus.example.mvvm.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.asus.example.R;
+
+public class LookupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lookup);
+    }
+}
