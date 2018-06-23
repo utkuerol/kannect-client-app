aab6cf9c18670e39198031c40e34f56b562eef38
FlorianGiner <florianginer1997@gmail.com>
2018-06-23 22:20:32 +0200

    doc

--- app/src/main/java/com/example/asus/example/mvvm/View/UserSearchResultAdapter.java
+++ app/src/main/java/com/example/asus/example/mvvm/View/UserSearchResultAdapter.java
@@ -1,44 +1,67 @@
 package com.example.asus.example.mvvm.View;
 
 import android.support.v7.widget.RecyclerView;
-
+import android.view.ViewGroup;
 import com.example.asus.example.R;
 
+/**
+ * Adapter class for the Recycler view. Class used to show all the  users, that match the typed in search query on the ui.
+ *
+ */
 public class UserSearchResultAdapter {
 
     //List<user> users
 
 
+    /**
+     * Constructor.
+     * initializes the List of Users who matched the typed in search Query.
+     */
     public UserSearchResultAdapter() {
         // create empty list
     }
 
+    /**
+     * Method inflates the View, meaning it creates the layout for every list item, using DataBindingutil inflate method.
+     * @param parent parent ViewGroup of the inflated view.
+     * @param viewType indicates what type of view should be inflated.
+     * @return a new instance of the UserSearchResultAdapterViewHoler.
+     */
     @Override
     public UserSearchResultAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        // create layout for every user item inside a view holder...
-
-        ItemPeopleBinding itemPeopleBinding =
-                DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.item_people,
-                        parent, false);
-        return new PeopleAdapterViewHolder(itemPeopleBinding);
+        return null;
     }
 
 
+    /**
+     * Method which binds a ViewHolder to a position in the Recycler View, using the bindUser method.
+     * @param holder ViewHolder which will be.
+     * @param position of the item in the list.
+     */
     @Override public void onBindViewHolder(PeopleAdapterViewHolder holder, int position) {
-        // binds user in the position with the nested class method
-        holder.bindPeople(peopleList.get(position));
     }
 
+    /**
+     * method to get the size of the List of users which will be shown in the ui.
+     * @return length of list
+     */
     @Override public int getItemCount() {
         return userList.size();
     }
 
-    public void setPeopleList(List<User> users) {
+    /**
+     * sets the list of user which will be shown in the ui.
+     * @param users list of users
+     */
+    public void setUserList(List<User> users) {
         this.userList = userList;
     }
 
 
+    /**
+     * Binds every List item
+     */
     public static class UserSearchResultAdapterViewHolder extends RecyclerView.ViewHolder {
 
         UserBinding UserBinding;
@@ -47,7 +70,7 @@ public class UserSearchResultAdapter {
 
         }
 
-        void bindUser(User user) {
+        public void bindUser(User user) {
             //creates view model for every user ...
 
             if (mItemPeopleBinding.getPeopleViewModel() == null) {
