155463d06579d08416ccf4639e1bc5b95642bda6
mohamedbayoudh <mohamed-bayoudh@outlook.de>
2018-06-23 18:26:34 +0200

    MainFeed Activity + VM

---
+++ app/src/main/java/com/example/asus/example/mvvm/ViewModel/MainFeedViewModel.java
@@ -0,0 +1,26 @@
+package com.example.asus.example.mvvm.ViewModel;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.example.asus.example.mvvm.Model.Entities.Post;
+import com.example.asus.example.mvvm.Model.Entities.User;
+import com.example.asus.example.mvvm.Model.Repository.FeedRepository;
+import com.example.asus.example.mvvm.Model.Repository.UserRepository;
+
+import java.util.List;
+
+public class MainFeedViewModel extends ViewModel {
+    private MutableLiveData<List<Post>> mFeed;
+    private MutableLiveData<User> mUser;
+
+    private FeedRepository feedRepository;
+
+    public MainFeedViewModel(MutableLiveData<User> user) {
+
+        this.feedRepository = new FeedRepository();
+        mFeed = feedRepository.getPersonalFeed(user.getValue());
+    }
+
+
+}
