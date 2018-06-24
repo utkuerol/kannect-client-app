6175f752b661da706c3412bd9eb1c833b5f97da2
utkuerol <utkuerol97@gmail.com>
2018-06-24 18:02:01 +0200

    viewmodels finished with javadocs and everything

--- app/src/main/java/com/example/asus/example/mvvm/ViewModel/UserViewModel.java
+++ app/src/main/java/com/example/asus/example/mvvm/ViewModel/UserViewModel.java
@@ -3,32 +3,61 @@ package com.example.asus.example.mvvm.ViewModel;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 
 import com.example.asus.example.mvvm.Model.Entities.User;
 import com.example.asus.example.mvvm.Model.Repository.UserRepository;
 
 import java.util.List;
 
-import javax.swing.text.View;
-
+/**
+ * ViewModel class for Users that is responsible for preparing and managing the data for Views,
+ * which need a list of users, by handling the communication of the View with the
+ * UserRepository class, which has the user business logic of the application.
+ * Objects received from repositories will be stored as MutableLiveData Objects.
+ */
 public class UserViewModel extends ViewModel {
+
 
     private MutableLiveData<List<User>> users;
     private UserRepository userRepository;
     private Context context;
 
+    /**
+     * Constructor to create a new instance with the given application context.
+     *
+     * @param context
+     */
     public UserViewModel(Context context) {
         this.context = context;
     }
 
+    /**
+     * Gets the users.
+     *
+     * @return observable list of users.
+     */
+    public MutableLiveData<List<User>> getUsers() {
+        return users;
+    }
+
+    /**
+     * Sets the list of users to search results with the given query.
+     * @param query to search for users.
+     */
     public void setUsersToSearchResults(String query) {
 
     }
+
+    /**
+     * Sets the list of users to subscriptions of the current user.
+     */
     public void setUsersToSubscriptions() {
 
     }
+
+    /**
+     * Sets the list of users to subscribers of the current user.
+     */
     public void setUsersToSubscribers() {
 
     }
