68dec7709952292e265deb7f7948103f3c20a493
mohamedbayoudh <mohamed-bayoudh@outlook.de>
2018-06-24 20:49:49 +0200

    JavaDoc

--- app/src/main/java/com/example/asus/example/mvvm/View/PersonalFeedActivity.java
+++ app/src/main/java/com/example/asus/example/mvvm/View/PersonalFeedActivity.java
@@ -8,12 +8,17 @@ import android.support.v7.app.AppCompatActivity;
 import com.example.asus.example.mvvm.Model.Entities.Event;
 import com.example.asus.example.mvvm.ViewModel.PostViewModel;
 
-//this is the FEED where the signed user has all Posts from the subscribed Entities // NO Profile ~mohamed
+/**
+ * Personal Feed Activity to show all Posts for this signed user of his groups , posts , events
+ */
 public class PersonalFeedActivity extends AppCompatActivity {
 
-    PostViewModel viewModel = new PostViewModel();
-    //event from intent
+    PostViewModel viewModel;
 
+
+    /**
+     * Is the method allowing to reach the activity
+     */
     public static Intent launchWithDetails(Context context, Event event) {
         // redirects from parameter context to this activity, takes user as parameter
         return null;
