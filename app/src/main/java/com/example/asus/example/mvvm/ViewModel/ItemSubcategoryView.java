7e56697e768d7d1acace57202c8ca380c92fefc4
utkuerol <utkuerol97@gmail.com>
2018-07-18 01:22:30 +0200

    item subcategory vm

--- app/src/main/java/com/example/asus/example/mvvm/ViewModel/ItemSubcategoryView.java
+++ app/src/main/java/com/example/asus/example/mvvm/ViewModel/ItemSubcategoryView.java
@@ -4,55 +4,73 @@ import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
 import com.example.asus.example.mvvm.Model.Entities.Subcategory;
+import com.example.asus.example.mvvm.Model.Repository.CategoryRepository;
 
 import javax.naming.Context;
 import javax.swing.text.View;
 
-
 /**
- * ViewModel class for itemSubcategory, that is responsible for preparing and managing the data for Views,
- * which need a list of itemSubcategory, by handling the communication of the View with the
- * Recyclerview element.
+ * ViewModel class for one specific subcategory, and is responsible for preparing and managing the data for Views,
+ * which need the information of this particular subcategory, by handling the communication of the View with the
+ * CategoryRepository class, which has the category and subcategory business logic of the application.
+ * Objects received from repositories will be stored as MutableLiveData Objects.
  */
 public class ItemSubcategoryView extends ViewModel {
 
     private MutableLiveData<Subcategory> subcategory;
     private Context context;
+    private CategoryRepository categoryRepository;
 
     /**
-     * Creates an instance with the given application context.
+     * Creates an instance with the given chosenSubcategory and application context.
      *
-     * @param context of the application.
+     * @param chosenSubcategory subcategory
+     * @param context           of the application.
      */
-    public ItemSubcategoryView(Subcategory subcategory, Context context) {
-        MutableLiveData<Subcategory> s = new MutableLiveData<Subcategory>();
-        this.subcategory = s;
+    public ItemSubcategoryView(Subcategory chosenSubcategory, Context context) {
+        this.subcategory = subcategory;
         this.context = context;
     }
 
     /**
-     * method will be invoked by clicking on the event
-     *
-     * @param view the current View
+     * Starts the SubcategoryEventPageActivity with the chosen subcategory.
+     * @param view
      */
     public void onItemClickEvent(View view) {
 
     }
 
     /**
-     * method will be invoked by clicking on the group
-     *
-     * @param view the current View
+     * Starts the SubcategoryGroupPageActivity with the chosen subcategory.
+     * @param view
      */
     public void onItemClickGroup(View view) {
 
     }
 
+    /**
+     * Gets the name of the subcategory.
+     * @return
+     */
     public String getName() {
     }
 
+    /**
+     * Creates a new group in the chosen subcategory with the user-given parameters.
+     * @param name of the group to be created.
+     * @param description of the group to be created.
+     * @param imageUrl of the group to be created.
+     */
     public void createGroup(String name, String description, String imageUrl) {
     }
+
+
+    /**
+     * Creates a new event in the chosen subcategory with the user-given parameters.
+     * @param name of the event to be created.
+     * @param description of the event to be created.
+     * @param imageUrl of the event to be created.
+     *//
 
     public void createEvent(String name, String description, String imageUrl) {
     }
