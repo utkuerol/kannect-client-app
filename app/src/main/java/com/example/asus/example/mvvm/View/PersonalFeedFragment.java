491a6a84ddb2a46f20db6ef30646d31a3ac2c77d
utkuerol <utkuerol97@gmail.com>
2018-07-22 17:24:16 +0200

    implementing personal feed fragment feature

--- app/src/main/java/com/example/asus/example/mvvm/View/PersonalFeedFragment.java
+++ app/src/main/java/com/example/asus/example/mvvm/View/PersonalFeedFragment.java
@@ -5,8 +5,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
+import com.example.asus.example.R;
+import com.example.asus.example.databinding.FragmentPersonalFeedBinding;
 import com.example.asus.example.mvvm.View.Adapter.PostAdapter;
 import com.example.asus.example.mvvm.ViewModel.PostViewModel;
 
@@ -16,16 +21,32 @@ import com.example.asus.example.mvvm.ViewModel.PostViewModel;
 public class PersonalFeedFragment extends Fragment {
 
     private PostViewModel postViewModel;
-    private PostAdapter adapter;
+
+    FragmentPersonalFeedBinding fragmentPersonalFeedBinding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.fragment_personal_feed, parent, false);
+    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
         super.onViewCreated(view, savedInstanceState);
 
+        //set viewmodel
         postViewModel = ViewModelProviders.of(this).get(PostViewModel.class);
         postViewModel.setPostsToPersonalFeed();
+
+        //set adapter
         PostAdapter postAdapter = new PostAdapter();
         postAdapter.setPostList(postViewModel.getPosts().getValue());
+        fragmentPersonalFeedBinding.personalFeedRV.setAdapter(postAdapter);
+        fragmentPersonalFeedBinding.personalFeedRV.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
+        //TODO: observe livedata somehow
+
     }
 
 
