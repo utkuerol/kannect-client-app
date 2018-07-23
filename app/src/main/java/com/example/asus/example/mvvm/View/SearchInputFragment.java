5e64546f4a6a6b698a92151d198c67f6bd06593c
utkuerol <utkuerol97@gmail.com>
2018-07-23 17:16:33 +0200

    bugfixes

--- app/src/main/java/com/example/asus/example/mvvm/View/SearchInputFragment.java
+++ app/src/main/java/com/example/asus/example/mvvm/View/SearchInputFragment.java
@@ -1,40 +1,25 @@
 package com.example.asus.example.mvvm.View;
 
-import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.asus.example.databinding.FragmentPersonalFeedBinding;
-import com.example.asus.example.mvvm.View.Adapter.PostAdapter;
-import com.example.asus.example.mvvm.ViewModel.PostViewModel;
+import com.example.asus.example.databinding.FragmentUserSearchInputBinding;
 
 public class SearchInputFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
 
-
-        //set viewmodel
-        PostViewModel postViewModel = ViewModelProviders.of(this).get(PostViewModel.class);
-        postViewModel.setPostsToPersonalFeed();
-
-        //set adapter
-        PostAdapter postAdapter = new PostAdapter();
-        postAdapter.setPostList(postViewModel.getPosts().getValue());
-
-
+        
         //set databinding
-        FragmentPersonalFeedBinding fragmentPersonalFeedBinding = FragmentPersonalFeedBinding.inflate(inflater, parent, false);
-        fragmentPersonalFeedBinding.personalFeedPostRV.setAdapter(postAdapter);
-        fragmentPersonalFeedBinding.personalFeedPostRV.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        FragmentUserSearchInputBinding fragmentUserSearchInputBinding = FragmentUserSearchInputBinding.inflate(inflater, parent, false);
 
         //TODO: observe livedata somehow
 
-        return fragmentPersonalFeedBinding.getRoot();
+        return fragmentUserSearchInputBinding.getRoot();
 
     }
 
