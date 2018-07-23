407bcab0232666d626cc937fd31e36f09d7691b4
FlorianGiner <florianginer1997@gmail.com>
2018-07-23 14:27:58 +0200

    renamed UserSearchInputfragment to SearchInputFragment

--- app/src/main/java/com/example/asus/example/mvvm/View/SearchInputFragment.java
+++ app/src/main/java/com/example/asus/example/mvvm/View/SearchInputFragment.java
@@ -6,7 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.View;
 
-public class UserSearchInputFragment extends Fragment {
+public class SearchInputFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
