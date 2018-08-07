bc9fdf1dc63e1366902b4fb7104743df314f1738
utkuerol <utkuerol97@gmail.com>
2018-08-08 01:17:42 +0200

    network

--- app/src/main/java/com/example/asus/example/mvvm/Model/WebServices/ServiceGenerator.java
+++ app/src/main/java/com/example/asus/example/mvvm/Model/WebServices/ServiceGenerator.java
@@ -11,7 +11,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ServiceGenerator {
 
     // better to store in Gradle String files
-    private static final String BASE_URL = "http://10.172.5.82:8080/";
+    private static final String BASE_URL = "http://10.0.2.2:8080/";
 
 
     private static Retrofit.Builder builder = new Retrofit.Builder()
